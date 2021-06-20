2a2deecddacd33610258b9ca3ea6f854651794cb
shanji97 <aleksander.kovac97@hotmail.com>
2021-06-20 15:55:03 +0200

    fotke se naložijo iz neta, če specificiramo pot
    fotke se naložijo iz neta, če specificiramo pot

--- mojaUL/app/src/main/java/si/uni/lj/fe/tnuv/mojaUL/seznamZaposlenih.java
+++ mojaUL/app/src/main/java/si/uni/lj/fe/tnuv/mojaUL/seznamZaposlenih.java
@@ -30,46 +30,44 @@ public class seznamZaposlenih extends AppCompatActivity {
 
         //sicer napolni statično
         //TU JE POTREBNO IMETI POVEZAVO, KI SE KONČA S fotka.jpg
-        seznamVsehZaposlenih.add( new Zaposlen("Janezek", "Bešter", "prof. dr.",
-                "BN511-KAB","014768431", "https://cdn.pixabay.com/photo/2021/05/24/12/17/rocks-6278936_960_720.jpg","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "));
 
         seznamVsehZaposlenih.add( new Zaposlen("Janez", "Bešter", "prof. dr.",
-                "BN511-KAB","014768431", "https://www.fe.uni-lj.si/mma_bin.php?id=2017061411062592&src=min","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "));
+                "BN511-KAB","+38614768431", "https://i.imgur.com/p454aZz.jpg","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "));
 
 
 
         seznamVsehZaposlenih.add( new Zaposlen("Jaka", "Sodnik", "prof. dr.",
-                "BN509-KAB","014768494", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073116103501&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN509-KAB","014768494", "https://i.imgur.com/nFLAPZ7.jpg","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Grega", "Jakus", "izr. prof. dr.",
-                "BN514/1-LKN","014768938", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073115544462&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","+38614768938", "https://i.imgur.com/QjJx0LC.jpg","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Anton", "Kos", "izr. prof. dr.",
-                "BN514/1-LKN","014768493", "https://www.fe.uni-lj.si/mma_bin.php?id=2016032210172019&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","014768493", "https://i.imgur.com/hHtyhQE.jpg","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
         //PODVOJIMO kar te vnose
         seznamVsehZaposlenih.add( new Zaposlen("Janez-2", "Bešter-2", "prof. dr.",
-                "BN511-KAB","014768431", "https://www.fe.uni-lj.si/mma_bin.php?id=2017061411062592&src=min","Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN511-KAB","+38614768431", "https://www.fe.uni-lj.si/mma_bin.php?id=2+38617061411062592&src=min","Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Jaka-2", "Sodnik-2", "prof. dr.",
-                "BN509-KAB","014768494", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073116103501&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN509-KAB","+38614768494", "https://www.fe.uni-lj.si/mma_bin.php?id=2+386140731161035+3861&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Grega-2", "Jakus-2", "izr. prof. dr.",
-                "BN514/1-LKN","014768938", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073115544462&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","+38614768938", "https://www.fe.uni-lj.si/mma_bin.php?id=2+38614073115544462&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Anton-2", "Kos-2", "izr. prof. dr.",
-                "BN514/1-LKN","01768493", "https://www.fe.uni-lj.si/mma_bin.php?id=2016032210172019&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","+3861768493", "https://www.fe.uni-lj.si/mma_bin.php?id=2+3861603221+386172+38619&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Janez-3", "Bešter-3", "prof. dr.",
-                "BN511-KAB","014768431", "https://www.fe.uni-lj.si/mma_bin.php?id=2017061411062592&src=min","Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN511-KAB","+38614768431", "https://www.fe.uni-lj.si/mma_bin.php?id=2+38617061411062592&src=min","Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Jaka-3", "Sodnik-3", "prof. dr.",
-                "BN509-KAB","014768494", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073116103501&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN509-KAB","+38614768494", "https://www.fe.uni-lj.si/mma_bin.php?id=2+386140731161035+3861&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Grega-3", "Jakus-3", "izr. prof. dr.",
-                "BN514/1-LKN","014768938", "https://www.fe.uni-lj.si/mma_bin.php?id=2014073115544462&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","+38614768938", "https://www.fe.uni-lj.si/mma_bin.php?id=2+38614073115544462&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
         seznamVsehZaposlenih.add( new Zaposlen("Anton-3", "Kos-3", "izr. prof. dr.",
-                "BN514/1-LKN","014768493", "https://www.fe.uni-lj.si/mma_bin.php?id=2016032210172019&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
+                "BN514/1-LKN","+38614768493", "https://www.fe.uni-lj.si/mma_bin.php?id=2016032210172019&src=min","2Na govorilnih urah ste dobrodošli po predhodnem dogovoru, na elektronsko pošto ali telefonsko številko"));
 
 
 
